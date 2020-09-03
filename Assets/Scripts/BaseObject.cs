ff2f04bd75ff70d1b53b692dae2e99c931702a10
tomashined <45771896+tomashined@users.noreply.github.com>
2020-09-03 23:09:44 +0300

    BaseObject и Flashlight

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+/// <summary>
+/// Базовый класс, кэширующий ссылки на компоненты объекта
+/// </summary>
+public abstract class BaseObject : MonoBehaviour
+{
+    private Transform _goTransform;
+    private GameObject _goInstance;
+    private string _name;
+
+    private Rigidbody _rigidbody;
+
+    private Material _material;
+    private Color _color;
+    private Animator _animator;
+
+    private bool _isVisible;
+
+    #region Properties
+    public Transform GoTransform { get => _goTransform; set => _goTransform = value; }
+    public GameObject GoInstance { get => _goInstance; set => _goInstance = value; }
+    public string Name { get => _name; set => _name = value; }
+    public Rigidbody Rigidbody { get => _rigidbody; set => _rigidbody = value; }
+    public Material Material { get => _material; set => _material = value; }
+    public Color Color { get => _color; set => _color = value; }
+    public Animator Animator { get => _animator; set => _animator = value; }
+    public bool IsVisible
+    {
+        get
+        {
+            return _isVisible;
+        }
+        set
+        {
+            _isVisible = value;
+        }
+    }
+    #endregion
+
+    protected virtual void Awake()
+    {
+        _goInstance = gameObject;
+        _goTransform = gameObject.transform;
+        _name = gameObject.name;
+
+        if (GetComponent<Rigidbody>())
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        else
+        {
+            Debug.Log("BaseObject: " + _name + " NO RIGIDBODY");
+            gameObject.AddComponent<Rigidbody>();
+        }
+
+        if (GetComponent<Animator>())
+        {
+            _animator = GetComponent<Animator>();
+        }
+        else
+        {
+            Debug.Log("BaseObject: " + _name + " NO Animator");
+            gameObject.AddComponent<Animator>();
+        }
+
+        if (GetComponent<Renderer>())
+        {
+            _material = GetComponent<Renderer>().material;
+        }
+    }
+}
---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : BaseObject
+{
+    [SerializeField] private Light FlashLight;
+    [SerializeField] private float timeout = 10;
+    private float currTime;
+    private float currReloadTime;
+
+    private KeyCode control = KeyCode.F;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        FlashLight = GetComponentInChildren<Light>();
+    }
+
+    private void ActiveFlashLight(bool val)
+    {
+        FlashLight.enabled = val;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(control))
+        {
+            ActiveFlashLight(!FlashLight.enabled);
+        }
+
+        if (FlashLight.enabled)
+        {
+            currTime += Time.deltaTime;
+            if (currTime > timeout)
+            {
+                currTime = 0;
+                ActiveFlashLight(false);
+            }
+        }
+    }
+}
