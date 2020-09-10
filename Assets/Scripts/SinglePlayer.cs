42e907b86d7e6b87e3d7b2a9cdb114708c4b1592
tomashined <45771896+tomashined@users.noreply.github.com>
2020-09-10 20:44:55 +0300

    Добавить контроллер для захвата объекта на сцене и перемещения его в пространстве (физическое перемещение или перемещение с привязкой).

---
+++ Assets/Scripts/SinglePlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinglePlayer : Unit
+{
+    RaycastHit hit;
+    Ray ray;
+    Transform McamT;
+    Transform target;
+    bool grab;
+
+    void Start()
+    {
+        Health = 100;
+        Dead = false;
+        McamT = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        ray = new Ray(McamT.position, McamT.forward);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            grab = !grab;
+        }
+        if (grab)
+        {
+            if (Physics.Raycast(ray, out hit, 50))
+            {
+                if (hit.collider.tag == "PickUp")
+                {
+                    hit.transform.GetComponent<Rigidbody>().isKinematic = true;
+                    target = hit.transform.parent;
+                    target.parent = McamT;
+                }
+            }
+        }
+        else
+        {
+            target.parent = null;
+            hit.transform.GetComponent<Rigidbody>().isKinematic = false;
+        }
+
+    }
+}
