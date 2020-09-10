05e79fd7d565777eef62fa1ac4e432d6dc7e3b9a
tomashined <45771896+tomashined@users.noreply.github.com>
2020-09-10 22:24:54 +0300

    Nav Mesh Agent

--- Assets/Scripts/Bot.cs
+++ Assets/Scripts/Bot.cs
@@ -1,20 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Bot : Unit
 {
-    private void Start()
+    private NavMeshAgent _agent;
+    private Transform _playerT;
+    private int stoppingDistance = 3;
+
+    protected override void Awake()
     {
+        base.Awake();
+
+        _agent = GetComponent<NavMeshAgent>();
+        _playerT = GameObject.FindObjectOfType<SinglePlayer>().transform;
         Health = 100;
         Dead = false;
     }
+
     private void Update()
     {
+        _agent.stoppingDistance = stoppingDistance;
+        _agent.SetDestination(_playerT.position);
+
+        if (_agent.remainingDistance > _agent.stoppingDistance)
+        {
+            GOAnimator.SetBool("move", true);
+        }
+        else
+        {
+            GOAnimator.SetBool("move", false);
+        }
+
         if (Dead)
         {
+            _agent.ResetPath();
             GORigidbody.isKinematic = true;
-            //Animation Die
+            GOAnimator.SetBool("die", true);
             Destroy(gameObject, 5f);
             return;
         }
